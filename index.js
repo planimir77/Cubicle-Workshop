00123658c892a1874f71bebad9580dd5b88565c2
planimir77 <planimir7708@gmail.com>
2020-10-09 12:23:53 +0300

    Remove unnecessary code

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const config = require('./config/config')[env];
 const express = require('express');
 const app = require('express')();
 
-// just for test
-const path = require('path');
-
 require('./config/express')(app, express);
 require('./config/routes')(app);
 
