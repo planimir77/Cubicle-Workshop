099ff929ef115c66c2ab120ac270a2bfe49675db
planimir77 <planimir7708@gmail.com>
2020-10-13 07:58:32 +0300

    Add error handler and success message

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const app = require('express')();
 const connectionStr = process.env.SoftuniDb;
 const mongoose = require('mongoose');
 
-
-mongoose.connect(connectionStr, { useNewUrlParser: true, useUnifiedTopology: true, });
+mongoose.connect(connectionStr,
+    { useNewUrlParser: true, useUnifiedTopology: true, },
+    (err) => {
+        if(err) console.log(err);
+        else console.log('Connected to the database')
+    });
 
 require('./config/express')(app, express);
 require('./config/routes')(app);
